c134e4d7a98b69e795c18331baf66e06e433eacf
Eric Pratt <ericprattusc@gmail.com>
2023-11-30 22:45:50 -0800

    working version of template

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useState} from "react";
+
+export default function App(){
+    return(
+        <div>
+            <p1> Hi i am a moron HAHAHAHAHAHAH HEHEHEHEHEHEHE  </p1>
+        </div>
+    )
+}
