a0cc28d6a5bb7f74bb672cd47998ebc45abb34a9
Eric Pratt <epstringskip@gmail.com>
2023-12-18 17:56:46 -0800

    fixed double rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,17 +15,7 @@ export default function Card(props){
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price} </span> / person</p>
         </div>
-        <div className="card">
-            <img src={props.img}></img>
-            <div className="card--stats">
-                <img src="../assets/images/star.png" className="card--star"></img>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
-            </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price} </span> / person</p>
-        </div>
+
         </> 
     )
 }
